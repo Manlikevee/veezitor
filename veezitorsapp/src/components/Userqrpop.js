e07cf4d4205b3ac2a23b266a210bf1c5732dfc44
manlikevee <odahviktor@gmail.com>
2024-06-18 00:46:12 +0100

    uuuuuuuu

--- veezitorsapp/src/components/Userqrpop.js
+++ veezitorsapp/src/components/Userqrpop.js
@@ -1,7 +1,7 @@
 
 'use client'
 import React from 'react'
-// import Linkqrcard from '@/components/qrcodecard/Linkqrcard';
+import Linkqrcard from '@/components/qrcodecard/Linkqrcard';
 
 
 const Userqrpop = ({data, togglemyqr}) => {
@@ -10,8 +10,8 @@ const Userqrpop = ({data, togglemyqr}) => {
   return (
     <div className="loading-over2 mytagqr" style={{ display: "flex" }}>
 <div className="containerqr">
-{/* <Linkqrcard data={data} /> */}
-{data}
+<Linkqrcard data={data} />
+
   <div className="textqr" onClick={togglemyqr}>
     <h1 className="headerqr">
       Veetec
