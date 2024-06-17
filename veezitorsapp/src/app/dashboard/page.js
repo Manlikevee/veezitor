23c919953f2d3902c0c7c2b733d4bbc81b663dd1
manlikevee <odahviktor@gmail.com>
2024-06-18 00:29:09 +0100

    ADADA

--- veezitorsapp/src/app/dashboard/page.js
+++ veezitorsapp/src/app/dashboard/page.js
@@ -14,17 +14,15 @@ import Cookies from "js-cookie";
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation'
 import Acceptvisit from "@/components/utility/Acceptvisit";
-
-const Dynamiccarp = dynamic(() => import('@/components/myqrrpopup'), { ssr: false });
 const ClearSearchParams = dynamic(() => import('@/components/utility/ClearSearchParams'), { ssr: false });
+
 const page = () => {
   const {visitordataloaded, isOpen, test, setTest,  fetchvisitors,  acceptvisitor, visitors , awaiting, pendingApproval, reshedule, inProgress, employeedataloaded, togglevisitorbar } = useContext(VeeContext);
   const [firstName, setFirstName] = useState("");
   const [myArray, setMyArray] = useState([]);
   const [activeTag, setActiveTag] = useState("allEmployeesContent");
   const [username, setUsername] = useState("");
-  const [showmypop, setshowmypop] = useState("");
-  const [weburl, setweburl] = useState("");
+
 
 
   const copyToClipboard = async () => {
@@ -34,11 +32,6 @@ const page = () => {
       const decodedToken = jwtDecode(accessdatatoken);
       const textToCopy = `${window.location.protocol}//${window.location.host}/visitation/newvisitation/${decodedToken?.data?.ref}`
       await navigator.clipboard.writeText(textToCopy);
-      setweburl(textToCopy)
-      if (decodedToken?.data?.ref){
-        handleshowpop()
-      }
-      
 
  toast.success('Copied To Clipboard')
     } catch (err) {
@@ -77,18 +70,12 @@ const page = () => {
   const handleTagClick = (targetId) => {
     setActiveTag(targetId);
   };
-  const handleshowpop = () => {
-    setshowmypop(!showmypop);
-  };
 
 
   return (
     <Dashboardlayout>
       <Acceptvisit togglevisitorbar={togglevisitorbar} acceptvisitor={acceptvisitor}  />
       {/* <Updateprofilemodal/> */}
-   {
-    showmypop && (<Dynamiccarp  data={weburl} handleshowpop={handleshowpop}/>)
-   }   
       <div className="sectionheader">
         <div className="sectiontitle col">
           <div>
