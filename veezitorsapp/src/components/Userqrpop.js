2ddb0ca4cf4db61fc9342d58720028da07fe962d
manlikevee <odahviktor@gmail.com>
2024-06-18 00:49:32 +0100

    yyyyyyyyy

--- veezitorsapp/src/components/Userqrpop.js
+++ veezitorsapp/src/components/Userqrpop.js
@@ -1,14 +1,29 @@
 
 'use client'
-import React from 'react'
+import React, { createRef } from "react";
 import Linkqrcard from '@/components/qrcodecard/Linkqrcard';
+import { useScreenshot, createFileName } from "use-react-screenshot";
 
 
 const Userqrpop = ({data, togglemyqr}) => {
+  const ref = createRef(null);
+  const [image, takeScreenShot] = useScreenshot({
+    type: "image/jpeg",
+    quality: 1.0
+  });
+
+  const download = (image, { name = "img", extension = "jpg" } = {}) => {
+    const a = document.createElement("a");
+    a.href = image;
+    a.download = createFileName(extension, name);
+    a.click();
+  };
+
+  const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
 
     
   return (
-    <div className="loading-over2 mytagqr" style={{ display: "flex" }}>
+    <div className="loading-over2 mytagqr" style={{ display: "flex" }} ref={ref}>
 <div className="containerqr">
 <Linkqrcard data={data} />
 
@@ -19,6 +34,7 @@ const Userqrpop = ({data, togglemyqr}) => {
     <p className="descriptionqr">
       Scan the QR code to start your visitation request
     </p>
+    <button onClick={downloadScreenshot}>Download screenshot</button>
   </div>
 </div>
 </div>
